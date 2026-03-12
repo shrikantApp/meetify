42f6e3c5d19fa48a529c10e3a063592d6ec60bf7
shrikantApp <shrikant@appristine.in>
2026-03-12 23:47:17 +0530

    pushing code of issue resolving

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/",
+  plugins: [react(), viteTsconfigPaths()],
+  optimizeDeps: {
+    include: ["react-icons/pi", "react-icons/fa6", "react-icons/vsc"],
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false
+  },
+});
